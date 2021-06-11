9802eb3e6603ba6c91bc548dfb0683e3e18f6a26
MinKyeong Choi <59993071+alsrud9909@users.noreply.github.com>
2021-06-11 15:07:44 +0900

    Update FirebaseInit.py

--- face_detection/FirebaseInit.py
+++ face_detection/FirebaseInit.py
@@ -2,10 +2,10 @@ from firebase_admin import credentials
 from firebase_admin import firestore, storage
 import firebase_admin
 
-cred = credentials.Certificate('persona-d1ed9-firebase-adminsdk-pyf6e-951437f2bf.json')
+cred = credentials.Certificate('')
 firebase_admin.initialize_app(cred, {
-  'projectId': 'persona-d1ed9',
-  'storageBucket': 'persona-d1ed9.appspot.com'
+  'projectId': '',
+  'storageBucket': ''
 })
 
 db = firestore.client()
